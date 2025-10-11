deef445a433795f03d575ecacdd315de42d69979
Renato Coutinho <futcout@gmail.com>
2025-10-11 16:27:43 +0000

    mesmo erro? 1:25:34 PM: build-image version: e8c4c0b200e9701a8a8825b9ff6

--- src/components/layout/footer.tsx
+++ src/components/layout/footer.tsx
@@ -4,7 +4,7 @@ import { Facebook, Instagram, Twitter } from 'lucide-react';
 import { Button } from '@/components/ui/button';
 import { Logo } from '@/components/ui/logo';
 
-const whatsappUrl = "https://wa.me/551142405253?text=Olá!%20Tudo%20bem?%20Gostaria%20de%20ser%20atendido%20e%20saber%20mais";
+const whatsappUrl = "https://wa.me/551142405253?text=Olá!%20Tudo%20bem?%20Gostaria%20de%20ser%20atendido%20e%20saber%20-mais";
 
 export function Footer() {
   const instagramUrl = "https://www.instagram.com/petestrelacrematorio/";
