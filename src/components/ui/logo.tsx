695d6762d17a26880db167c68ffabd9ef1ceee24
Renato Coutinho <futcout@gmail.com>
2025-10-13 12:46:42 +0000

    aumente mais a imagem  (_for element img_)

--- src/components/ui/logo.tsx
+++ src/components/ui/logo.tsx
@@ -4,13 +4,13 @@ import Image from 'next/image';
 
 export const Logo = () => (
     <Link href="/" className="inline-block">
-        <div className="relative w-[140px] h-[40px]">
+        <div className="relative w-[180px] h-[50px]">
             <Image 
                 src="https://i.imgur.com/vkU2IlK.png" 
                 alt="Pet Estrela Crematório Logo" 
                 fill
                 className="object-contain"
-                sizes="140px"
+                sizes="180px"
                 priority 
             />
         </div>
