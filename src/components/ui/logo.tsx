11e63aa20ea5017e094a6f70d9178664cb0414f1
Renato Coutinho <futcout@gmail.com>
2025-10-13 13:18:47 +0000

    PRECISO QUE DE MAIS ZOOM, AUMENTE MAIS AINDA  (_for element img_)

--- src/components/ui/logo.tsx
+++ src/components/ui/logo.tsx
@@ -4,13 +4,13 @@ import Image from 'next/image';
 
 export const Logo = () => (
     <Link href="/" className="inline-block">
-        <div className="relative w-[200px] h-[56px]">
+        <div className="relative w-[250px] h-[70px]">
             <Image 
                 src="https://i.imgur.com/vkU2IlK.png" 
                 alt="Pet Estrela Crematório Logo" 
                 fill
                 className="object-contain"
-                sizes="200px"
+                sizes="250px"
                 priority 
             />
         </div>
