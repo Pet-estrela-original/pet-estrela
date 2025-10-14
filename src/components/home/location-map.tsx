12a9da3158ab886abcdfcd229e4cca345e77e525
Renato Coutinho <futcout@gmail.com>
2025-10-14 17:23:04 +0000

    nesse link está o endereço correto, substitua por esse link https://www.

--- src/components/home/location-map.tsx
+++ src/components/home/location-map.tsx
@@ -5,6 +5,10 @@ import Link from "next/link";
 import { Button } from "@/components/ui/button";
 
 export function LocationMap() {
+    const embedUrl = "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d915.2936733220551!2d-46.35338483036494!3d-23.40312829658532!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x94ce87f03e5696d1%3A0xe343b94849758421!2sAv.%20Ad%C3%ADlia%20Barbosa%20Neves%2C%202500-2682%20-%20Centro%20Industrial%2C%20Aruj%C3%A1%20-%20SP%2C%2007432-575!5e0!3m2!1spt-BR!2sbr!4v1721159847585!5m2!1spt-BR!2sbr";
+    const placeUrl = "https://www.google.com/maps/place/Av.+Ad%C3%ADlia+Barbosa+Neves,+2500-2682+-+Centro+Industrial,+Aruj%C3%A1+-+SP,+07432-575/@-23.4031283,-46.3540108,17z/data=!3m1!4b1!4m6!3m5!1s0x94ce87f03e5696d1:0xe343b94849758421!8m2!3d-23.4031283!4d-46.3527931!16s%2Fg%2F11h76dw5cg?entry=ttu";
+    const directionsUrl = "https://www.google.com/maps/dir//Av.+Ad%C3%ADlia+Barbosa+Neves,+2500-2682+-+Centro+Industrial,+Aruj%C3%A1+-+SP,+07432-575/@-23.4031283,-46.3540108,17z/data=!4m9!4m8!1m0!1m5!1m1!1s0x94ce87f03e5696d1:0xe343b94849758421!2m2!1d-46.3527931!2d-23.4031283!3e0?entry=ttu";
+
     return (
         <section className="py-20 lg:py-28 bg-white">
             <div className="container mx-auto max-w-7xl px-4">
@@ -16,7 +20,7 @@ export function LocationMap() {
                 </div>
                 <div className="aspect-video w-full rounded-lg overflow-hidden border shadow-lg">
                      <iframe
-                        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3661.1685329881093!2d-46.33158062375836!3d-23.41801915647565!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x94ce7b1e2e987979%3A0x88504997063a4362!2sAv.%20Ad%C3%ADlia%20Barbosa%20Neves%2C%202740%20-%20Centro%20Industrial%2C%20Aruj%C3%A1%20-%20SP%2C%2007432-575!5e0!3m2!1spt-BR!2sbr!4v1719262963162!5m2!1spt-BR!2sbr"
+                        src={embedUrl}
                         width="100%"
                         height="100%"
                         style={{ border: 0 }}
@@ -29,12 +33,12 @@ export function LocationMap() {
                     <p className="text-muted-foreground">Av. Adília Barbosa Neves, 2500-2682 - Centro Industrial</p>
                     <div className="flex justify-center gap-4">
                         <Button asChild>
-                            <Link href="https://www.google.com/maps/dir//Av.+Ad%C3%ADlia+Barbosa+Neves,+2740+-+Centro+Industrial,+Aruj%C3%A1+-+SP,+07432-575/@-23.4180192,-46.3315806,17z/data=!4m8!4m7!1m0!1m5!1m1!1s0x94ce7b1e2e987979:0x88504997063a4362!2m2!1d-46.3290057!2d-23.418024" target="_blank">
+                            <Link href={directionsUrl} target="_blank">
                                 Rotas
                             </Link>
                         </Button>
                         <Button asChild variant="outline">
-                             <Link href="https://www.google.com/maps/place/Av.+Ad%C3%ADlia+Barbosa+Neves,+2740+-+Centro+Industrial,+Aruj%C3%A1+-+SP,+07432-575/@-23.4180192,-46.3315806,17z/data=!3m1!4b1!4m6!3m5!1m1!1s0x94ce7b1e2e987979:0x88504997063a4362!2m2!1d-46.3290057!2d-23.418024" target="_blank">
+                             <Link href={placeUrl} target="_blank">
                                 Ver mapa ampliado
                             </Link>
                         </Button>
